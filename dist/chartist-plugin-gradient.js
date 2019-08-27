9947a132d1daf2ebe65c6eb736b3e40fdbcb6229
nigelflippo <flippo.nigel@gmail.com>
2019-08-26 23:53:18 -0600

    Updates build files

--- dist/chartist-plugin-gradient.js
+++ dist/chartist-plugin-gradient.js
@@ -115,12 +115,14 @@
 
   	Chartist.plugins = Chartist.plugins || {};
   	Chartist.plugins.ctAreaGradient = function (options) {
+
   		function uuid() {
   		  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
   		    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
   		    return v.toString(16);
   		  });
   		}
+
   		var maskIds = {
   			maskAboveId: uuid(),
   			maskBelowId: uuid()
